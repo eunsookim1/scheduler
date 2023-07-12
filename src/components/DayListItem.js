958c4e215eb2c014f67be0e41b37f145502d9d75
eunsookim1 <92eunsoo@gmail.com>
2023-07-11 21:52:10 -0700

    update classNames in the buttons file

---
+++ src/components/DayListItem.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./DayListItem.scss";
+import classNames from "classnames";
+
+export default function DayListItem(props) {
+  const day = props.name;
+
+  const dayClass = classNames('day-list__item', {
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': props.spots === 0
+  });
+
+  return (
+    <li onClick={() => props.setDay(day)} className={dayClass}>
+      <h2 className="text--regular" >{day}</h2> 
+      <h3 className="text--light">{props.spots} spots remaining</h3>
+    </li>
+  );
+}
