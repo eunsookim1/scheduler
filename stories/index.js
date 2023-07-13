c0ed0c919a5f99d0eeb9fd12fb319633c3d103ce
eunsookim1 <92eunsoo@gmail.com>
2023-07-12 23:11:09 -0700

    update stories index file accordingly

--- stories/index.js
+++ stories/index.js
@@ -7,8 +7,12 @@ import "index.scss";
 
 import Button from "components/Button";
 import DayListItem from "components/DayListItem";
+import DayList from "components/DayList";
+import InterviewerListItem from "components/InterviewerListItem";
+import InterviewerList from "components/InterviewerList";
 
 
+//---------- Button: 
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -26,11 +30,13 @@ storiesOf("Button", module)
   ));
 
 
+
+//---------- DayListItem:
 storiesOf("DayListItem", module)
 .addParameters({
   backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
 })
-.add("Unselected", () => <DayListItem name="Monday" spots={5} />) 
+.add("Unselected", () => <DayListItem setDay={action("setDay")} name="Monday" spots={5} />) 
 
 .add("Selected", () => <DayListItem name="Monday" spots={5} selected />) 
 
@@ -39,3 +45,112 @@ storiesOf("DayListItem", module)
 .add("Clickable", () => (
   <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} />
 ));
+
+
+
+
+//---------- DayList: 
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
+
+storiesOf("DayList", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
+  })
+  .add("Monday", () => (
+    <DayList days={days} day={"Monday"} setDay={action("setDay")} />
+  ))
+  .add("Tuesday", () => (
+    <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
+  ))
+  .add("Wednesday", () => (
+      <DayList days={days} day={"Wednesday"} setDay={action("setDay")} />
+  ));
+
+
+
+  
+// InterviewerListItem:
+
+const interviewer = {
+  id: 1,
+  name: "Sylvia Palmer",
+  avatar: "https://i.imgur.com/LpaY82x.png"
+};
+  
+  storiesOf("InterviewerListItem", module)
+    .addParameters({
+      backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+    })
+    .add("Unselected", () => (
+      <InterviewerListItem
+        id={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+      />
+    ))
+    .add("Selected", () => (
+      <InterviewerListItem
+        id={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected
+      />
+    ))
+    .add("Clickable", () => (
+      <InterviewerListItem
+        id={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        setInterviewer={action("setInterviewer")}
+      />
+    ));
+
+
+//---------- InterviewerList:
+
+const interviewers = [
+  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+];
+
+
+storiesOf("InterviewerList", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Initial", () => (
+    <InterviewerList
+      interviewers={interviewers}
+    />
+  ))
+  .add("Selected", () => (
+    <InterviewerList
+      interviewers={interviewers}
+      interviewer={3}
+    />
+  ))
+  .add("Clickable", () => (
+    <InterviewerList
+      interviewers={interviewers}
+      setInterviewer={action("setInterviewer")}
+    />
+  ));
