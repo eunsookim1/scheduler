3367cc14aa9e0d33186acbc18453a2859eefd472
eunsookim1 <92eunsoo@gmail.com>
2023-07-15 11:57:28 -0700

    add appointment empty

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -18,6 +18,7 @@ import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status"
 import Error from "components/Appointment/Error";
 import Form from "components/Appointment/Form";
+
 
 //---------- Button: 
 storiesOf("Button", module)
@@ -215,4 +216,14 @@ storiesOf("Appointment", module)
       onSave={action("onSave")}
       onCancel={action("onCancel")}
     />  
+  ))
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment 
+        id={1} 
+        time="4pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }} 
+      />
+      <Appointment time="5pm" />
+    </Fragment>
   ))
