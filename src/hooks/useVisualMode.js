2c53488e5dbd7508be64bf6895af96133eb9c3f2
eunsookim1 <92eunsoo@gmail.com>
2023-07-20 20:19:45 -0700

    add Hook functions

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+export default function useVisualMode (initial) {
+  const [mode, setMode] = useState(initial)
+  const [history, setHistory] = useState([initial]);
+
+  function transition (newMode, replace = false) {
+    if (replace) {
+      setMode(newMode)
+      setHistory(prevHistory => [...prevHistory.slice(0,-1), newMode]);
+    } else {
+      setMode(newMode)
+      setHistory(prevHistory => [...prevHistory, newMode]);
+    }
+  }
+
+  function back() {
+  
+    if (history.length > 1) { 
+      // Only go back if there's more than one mode in history
+      const newHistory = history.slice(0,-1); 
+      // Remove the last mode from history
+      setMode(newHistory[newHistory.length -1]); 
+      // Set the mode to the previousone
+      setHistory(newHistory);
+    }
+  }
+
+
+  return { mode, transition, back }
+};
