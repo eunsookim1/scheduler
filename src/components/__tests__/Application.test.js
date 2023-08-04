8f706cf54c0d333ed095a513162bf2741dabc988
eunsookim1 <92eunsoo@gmail.com>
2023-08-03 21:00:16 -0700

    add a new test for changing the schedule when a new day is selected

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,23 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
+it("changes the schedule when a new day is selected", async () => {
+  const { getByText } = render(<Application />);
+
+  await waitForElement(() => getByText("Monday"));
+
+  fireEvent.click(getByText("Tuesday"));
+
+  expect(getByText("Leopold Silvers")).toBeInTheDocument();
 });
+
+
+// I no longer use .then anymore and have an await in front of it.
+// When i don't have any result from the function I don't necessarily have to capture anything with the await. Not assigning a const or variable.
+// I'm returning a promise in a regular JS function.
+// await only works for asynchronous functions.
