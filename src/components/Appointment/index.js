722061cf1ab4da8cbc8e65efac308850dec7b7b8
eunsookim1 <92eunsoo@gmail.com>
2023-08-03 22:49:58 -0700

    adding a data-testid=appointment prop to <article> root element

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -69,7 +69,7 @@ export default function Appointment(props) {
 
   return (
     <>
-      <article className="appointment">
+      <article className="appointment" data-testid="appointment">
         <Header time={props.time}></Header>
 
         {/* Instead of using 'props.interview? <Show /> : <Empty />' I can use mode. Use the mode to conditionally render the component */}
