57434b44142b5676fcba5a8eabbb279c374009b5
eunsookim1 <92eunsoo@gmail.com>
2023-08-05 10:59:56 -0700

    add data-testid for testing

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -7,28 +7,29 @@ import classNames from "classnames";
 const formatSpots = (spots) => {
 
   return (
-    spots === 0 ? `no spots remaining` : spots === 1 ? 
-    `1 spot remaining` : `${spots} spots remaining`
-    )
+    spots === 0 ? `no spots remaining` : spots === 1 ?
+      `1 spot remaining` : `${spots} spots remaining`
+  )
 };
 
 export default function DayListItem(props) {
   const day = props.name;
-  
+
   const dayClass = classNames('day-list__item', {
     'day-list__item--selected': props.selected,
     'day-list__item--full': props.spots === 0
   });
-  
+
   const availabilityMessage = formatSpots(props.spots);
 
   return (
-    <li 
-    onClick={() => props.setDay(day)} 
-    className={dayClass}
-    selected={props.selected}
+    <li
+      onClick={() => props.setDay(day)}
+      className={dayClass}
+      selected={props.selected}
+      data-testid="day"
     >
-      <h2 className="text--regular" >{day}</h2> 
+      <h2 className="text--regular" >{day}</h2>
       <h3 className="text--light">{availabilityMessage}</h3>
     </li>
   );
